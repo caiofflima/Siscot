addbd4ffc875947acb82ef629c2dd2419d376a7c
Caio Freitas Ferreira de Lima <caio.lima@estudante.iftm.edu.br>
2023-06-03 14:58:05 -0300

    feat: auth by cookie

---
+++ src/middlewares/authbycookie.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const { Usuario } = require('../db');
+
+module.exports = async (req, res, next) => {
+
+    const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token não fornecido' });
+  }
+
+  try {
+    const payload = jwt.verify(token, 'secret');
+
+    const usuario = await Usuario.findByPk(payload.id);
+
+    if (!usuario) {
+      return res.status(401).json({ message: 'Usuário não encontrado' });
+    }
+
+    req.usuario = usuario;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Token inválido' });
+  }
+};
