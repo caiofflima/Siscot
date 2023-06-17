1353815f8cadb321131715478d2cecfb5a85041d
viniciusrs1 <contatoviniciusrs@gmail.com>
2023-06-17 16:55:30 -0300

    fix: ajustes login

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,11 +1,16 @@
-const UsuarioService = require('../services/UsuarioService');
+const UsuarioService = require("../services/UsuarioService");
 const usuarioService = new UsuarioService();
 
 module.exports = {
   async registro(req, res) {
     const { name, email, password, role } = req.body;
 
-    const usuario = await usuarioService.create({ name, email, password, role });
+    const usuario = await usuarioService.create({
+      name,
+      email,
+      password,
+      role,
+    });
 
     return res.json(usuario);
   },
@@ -15,23 +20,24 @@ module.exports = {
 
     const usuario = await usuarioService.findOne(email);
 
-
     if (!usuario || !(await usuario.checkPassword(password))) {
-      return res.status(401).json({ message: 'Email ou senha inválidos' });
+      return res.status(401).json({ message: "Email ou senha inválidos" });
     }
 
     const token = usuario.generateToken();
 
-    res.cookie('token', token, { httpOnly: true, secure: false, maxAge: 86400 });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      maxAge: 86400,
+    });
 
-    return res.json({ usuario, token});
+    return res.json({ usuario, token });
   },
 
   async logout(req, res) {
+    res.clearCookie("token");
 
-    res.clearCookie('token');
-
-    return res.json({ message: 'Logout realizado com sucesso' });
-  }
-  
+    return res.json({ message: "Logout realizado com sucesso" });
+  },
 };
--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -1,37 +1,42 @@
-const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 class Usuario extends Model {
   async checkPassword(password) {
-    return bcrypt.compare(password, this.password);
+    const response = await bcrypt.compare(password, this.password);
+    console.log("aq", response);
+    return response;
   }
 
   generateToken() {
-    return jwt.sign({ id: this.id }, 'secret' , { expiresIn: 86400 });
+    return jwt.sign({ id: this.id }, "secret", { expiresIn: 86400 });
   }
 }
 
 module.exports = (sequelize) => {
-  Usuario.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    role: DataTypes.ENUM("ADMIN", "ASSISTENTE SOCIAL", "SECRETARIO"),
-  }, {
-    sequelize,
-    modelName: "Usuario",
-    hooks: {
-      beforeCreate: async (usuario) => {
-        const hashedPassword = await bcrypt.hash(usuario.password, 10);
-        usuario.password = hashedPassword;
-      },
-      beforeUpdate: async (usuario) => {
-        const hashedPassword = await bcrypt.hash(usuario.password, 10);
-        usuario.password = hashedPassword;
-      }
+  Usuario.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      role: DataTypes.ENUM("ADMIN", "ASSISTENTE SOCIAL", "SECRETARIO"),
     },
-  });
+    {
+      sequelize,
+      modelName: "Usuario",
+      hooks: {
+        beforeCreate: async (usuario) => {
+          const hashedPassword = await bcrypt.hash(usuario.password, 10);
+          usuario.password = hashedPassword;
+        },
+        beforeUpdate: async (usuario) => {
+          const hashedPassword = await bcrypt.hash(usuario.password, 10);
+          usuario.password = hashedPassword;
+        },
+      },
+    }
+  );
 
   return Usuario;
 };
